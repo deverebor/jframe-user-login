7e1ef93389069b293bb373d47beec71a126b2994
Lucas Souza <lucasp.sdev@gmail.com>
2022-05-09 15:39:23 -0300

    test(User): better structure

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -8,33 +8,44 @@ public class UserTest {
     
     @Test
     public void shouldCreateUser() throws UserException {
-        User user = new User("Jhon Doe", "1234");
-        assertEquals("Jhon Doe", user.getUserName());
-        assertEquals("1234", user.getUserPassword());
+        String userName = "Jhon Doe";
+        String userPassword = "1234";
+        
+        User user = new User(userName, userPassword);
+        
+        assertEquals(userName, user.getUserName());
+        assertEquals(userPassword, user.getUserPassword());
     }
     
     @Test
     public void shouldThrowAnExceptionWhenCreateUserWithInvalidValues() {
         
+        String userNameOrPasswordEmptyException = "User name or password cannot be empty";
+        String userNameOrPasswordNullException = "User name or password cannot be null";
+        
         UserException userNameEmptyException = assertThrows(UserException.class, () -> {
             User user = new User("", "1234");
         });
-        assertEquals("User name or password cannot be empty", userNameEmptyException.getMessage());
+        
+        assertEquals(userNameOrPasswordEmptyException, userNameEmptyException.getMessage());
     
         UserException userPasswordEmptyException = assertThrows(UserException.class, () -> {
             User user = new User("Jhon Doe", "");
         });
-        assertEquals("User name or password cannot be empty", userPasswordEmptyException.getMessage());
+        
+        assertEquals(userNameOrPasswordEmptyException, userPasswordEmptyException.getMessage());
     
         UserException userPasswordNullException = assertThrows(UserException.class, () -> {
             User user = new User("Jhon Doe", null);
         });
-        assertEquals("User name or password cannot be null", userPasswordNullException.getMessage());
+        
+        assertEquals(userNameOrPasswordNullException, userPasswordNullException.getMessage());
     
         UserException userNameNullException = assertThrows(UserException.class, () -> {
             User user = new User(null, "1231");
         });
-        assertEquals("User name or password cannot be null", userNameNullException.getMessage());
+        
+        assertEquals(userNameOrPasswordNullException, userNameNullException.getMessage());
         
     }
     
